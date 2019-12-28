035bad53842192ef88e6190c8bb7dc67f189669a
alexbelan1999 <alexbelan1999@gmail.com>
2019-12-28 14:35:14 +0300

    Add рисунок11,12

--- picture.py
+++ picture.py
@@ -379,3 +379,122 @@ plt.xlabel('Время')
 plt.ylabel('Работы')
 plt.show()
 fig5.savefig("рисунок 10.png", dpi=300, qualite=100)
+
+fig6 = plt.figure()
+ax6 = fig6.add_subplot(1, 1, 1)
+x = np.arange(6, 18 + step, step) #p01
+x1 = np.arange(15, 24 + step, step) #p02
+x2 = np.arange(33, 45 + step, step) #p06
+x3 = np.arange(18, 28 + step, step) #p13
+x4 = np.arange(24, 28 + step, step) #p24
+x5 = 28 #p34
+x6 = np.arange(40, 48 + step, step) #p37
+x7 = np.arange(28, 37 + step, step) #p45
+x8 = np.arange(37, 45 + step, step) #p56
+x9 = np.arange(43, 48 + step, step) #p57
+x10 = np.arange(45, 48 + step, step) #p67
+
+y = 1 + x * 0
+y1 = 2 + x1 * 0
+y2 = 3 + x2 * 0
+y3 = 4 + x3 * 0
+y4 = 5 + x4 * 0
+y5 = 6 + x5 * 0
+y6 = 7 + x6 * 0
+y7 = 8 + x7 * 0
+y8 = 9 + x8 * 0
+y9 = 10 + x9 * 0
+y10 = 11 + x10 * 0
+
+plt.text(6, 1.1, '(p(0),p(1) 5', fontsize=fontsize)
+ax6.plot(x, y, 'k')
+plt.text(15, 2.1, '(p(0),p(2) 2', fontsize=fontsize)
+ax6.plot(x1, y1, 'k')
+plt.text(33, 3.1, '(p(0),p(6) 3', fontsize=fontsize)
+ax6.plot(x2, y2, 'k')
+plt.text(18, 4.1, '(p(1),p(3) 9', fontsize=fontsize)
+ax6.plot(x3, y3, 'k')
+plt.text(24, 5.1, '(p(2),p(4) 1', fontsize=fontsize)
+ax6.plot(x4, y4, 'k')
+plt.text(28, 6.1, '(p(3),p(4)', fontsize=fontsize)
+ax6.plot(x5, y5, 'k.')
+plt.text(40, 7.1, '(p(3),p(7) 4', fontsize=fontsize)
+ax6.plot(x6, y6, 'k')
+plt.text(28, 8.1, '(p(4),p(5) 2', fontsize=fontsize)
+ax6.plot(x7, y7, 'k')
+plt.text(37, 9.1, '(p(5),p(6) 5', fontsize=fontsize)
+ax6.plot(x8, y8, 'k')
+plt.text(43, 10.1, '(p(5),p(7) 5', fontsize=fontsize)
+ax6.plot(x9, y9, 'k')
+plt.text(45, 11.1, '(p(6),p(7) 3', fontsize=fontsize)
+ax6.plot(x10, y10, 'k')
+plt.minorticks_on()
+plt.title('Рисунок 7')
+plt.grid(True)
+
+plt.xticks(np.arange(0, 52, 2), np.arange(0, 52, 2))
+plt.yticks(np.arange(0, 13, 1), np.arange(0, 13, 1))
+plt.xlabel('Время')
+plt.ylabel('Работы')
+plt.show()
+fig6.savefig("рисунок 11.png", dpi=300, qualite=100)
+
+fig7 = plt.figure()
+ax7 = fig7.add_subplot(1, 1, 1)
+x = np.arange(0, 9 + step, step)
+x1 = np.arange(9, 12 + step, step)
+x2 = np.arange(12, 22 + step, step)
+x3 = np.arange(22, 26 + step, step)
+x4 = np.arange(26, 30 + step, step)
+x5 = np.arange(30, 34 + step, step)
+x6 = np.arange(34, 35 + step, step)
+x7 = np.arange(35, 43 + step, step)
+x8 = np.arange(43, 46 + step, step)
+x9 = np.arange(46, 48 + step, step)
+x10 = np.arange(0, 52, 1)
+
+y = 7 + x * 0
+y1 = 5 + x1 * 0
+y2 = 9 + x2 * 0
+y3 = 8 + x3 * 0
+y4 = 9 + x4 * 0
+y5 = 5 + x5 * 0
+y6 = 2 + x6 * 0
+y7 = 5 + x7 * 0
+y8 = 8 + x8 * 0
+y9 = 5 + x9 * 0
+y10 = 8.9 + x10 * 0
+
+plt.text(0, 7.1, '7', fontsize=fontsize)
+ax7.plot(x, y, 'k')
+plt.text(9, 5.1, '5', fontsize=fontsize)
+ax7.plot(x1, y1, 'k')
+plt.text(12, 9.1, '9', fontsize=fontsize)
+ax7.plot(x2, y2, 'k')
+plt.text(22, 8.1, '8', fontsize=fontsize)
+ax7.plot(x3, y3, 'k')
+plt.text(26, 9.1, '9', fontsize=fontsize)
+ax7.plot(x4, y4, 'k')
+plt.text(30, 5.1, '5', fontsize=fontsize)
+ax7.plot(x5, y5, 'k')
+plt.text(34, 2.1, '2', fontsize=fontsize)
+ax7.plot(x6, y6, 'k')
+plt.text(35, 5.1, '5', fontsize=fontsize)
+ax7.plot(x7, y7, 'k')
+plt.text(43, 8.1, '8', fontsize=fontsize)
+ax7.plot(x8, y8, 'k')
+plt.text(46, 5.1, '5', fontsize=fontsize)
+ax7.plot(x9, y9, 'k')
+plt.text(40, 9.1, 'R = 9', fontsize=fontsize)
+ax7.plot(x10, y10, 'k--')
+
+plt.minorticks_on()
+plt.title('Рисунок 8')
+plt.grid(True)
+
+plt.xticks(np.arange(0, 52, 2), np.arange(0, 52, 2))
+plt.yticks(np.arange(0, 13, 1), np.arange(0, 13, 1))
+plt.xlabel('Время')
+plt.ylabel('Суммарное потребление')
+plt.show()
+fig7.savefig("рисунок 12.png", dpi=300, qualite=100)
